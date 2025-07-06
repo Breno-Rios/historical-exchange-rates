4e3114b1a1a7e56d08fcc056f25237a4986795da
Breno Rios <brenooliveirarios@gmail.com>
2025-07-06 18:37:57 -0300

    teste(testes.py): add tests for insert_rate()

--- rates/tests.py
+++ rates/tests.py
@@ -1,3 +1,10 @@
+#https://developer.mozilla.org/pt-BR/docs/Learn_web_development/Extensions/Server-side/Django/Testing
 from django.test import TestCase
+from rates.models import Rate
+from datetime import date
 
 # Create your tests here.
+
+class RateModelTest(TestCase):
+  def test(self):
+    self.Rate = Rate.objects.create(date= date(2025, 7, 5), base= 'USD', currency = 'BRL' ,value = 2.00)
