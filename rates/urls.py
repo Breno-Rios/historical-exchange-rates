c8ad22955014c37b79c6e65d2cc2dddefef330af
Breno Rios <brenooliveirarios@gmail.com>
2025-07-08 20:21:06 -0300

    fix(urls) rename the urls

--- rates/urls.py
+++ rates/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('home/api/', views.call_get_rates, name= "rate_graph"),
 
     #New Endpoints
-    path('api/rates/period/', views.get_rates_by_range_date, name= "rates_periods"),
-    path('api/rates/day/', views.get_rates_by_day, name= "rates_for_day"),
+    path('api/rates/period/', views.get_rates_by_range_date, name= "rate_periods"),
+    path('api/rates/day/', views.get_rates_by_day, name= "day_rates"),
     path('api/rate/', views.get_rates_by_target, name= "rates_for_target"),
     path('api/rates/', views.get_all_rates, name= "all_rates"),
 ]
