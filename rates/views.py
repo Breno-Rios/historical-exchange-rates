fe182272f1c9910584a300ed001da05c7e696987
Breno Rios <brenooliveirarios@gmail.com>
2025-07-08 20:17:39 -0300

    feat(controller): implement view functions delegating to services

--- rates/views.py
+++ rates/views.py
@@ -43,3 +43,44 @@ def call_get_rates(request):
     except Exception as e:
         return JsonResponse({'error': 'Internal server error'}, status=500)
 
+def get_rates_by_range_date(request):
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+    if not all([start_date, end_date]):
+        return JsonResponse({'error': 'Required parameters missing'}, status=400)
+    try:
+        return JsonResponse({"message": "range"}, status=200)
+    except ValueError as e:
+        return JsonResponse({'error': str(e)}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': 'Internal server error'}, status=500)
+    
+def get_rates_by_target(request):
+    target = request.GET.get('target')
+    if not all([target]):
+        return JsonResponse({'error': 'Required parameters missing'}, status=400)
+    try:
+        return JsonResponse({"message": "target"}, status=200)
+    except ValueError as e:
+        return JsonResponse({'error': str(e)}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': 'Internal server error'}, status=500)
+    
+def get_rates_by_day(request):
+    start_date = request.GET.get('start_date')
+    if not all([start_date]):
+        return JsonResponse({'error': 'Required parameters missing'}, status=400)
+    try:
+        return JsonResponse({"message": "day"}, status=200)
+    except ValueError as e:
+        return JsonResponse({'error': str(e)}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': 'Internal server error'}, status=500)
+    
+def get_all_rates(request):
+    try:
+        return JsonResponse({"message": "all"}, status=200)
+    except ValueError as e:
+        return JsonResponse({'error': str(e)}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': 'Internal server error'}, status=500)
