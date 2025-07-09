c7f63454cc6e6cbf43b023018448cafda15ded80
Breno Rios <brenooliveirarios@gmail.com>
2025-07-09 16:02:50 -0300

    chore(settings): configure DEBUG, static files, and allowed hosts for production

--- historical_exchange_rates/settings.py
+++ historical_exchange_rates/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vu#sb=dg(5n(=t)xo9^9bpy%)l)ugs9-l_3ysk9&2)z_b-wbd1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 LOGGING = {
     'version': 1,
@@ -67,7 +67,7 @@ LOGGING = {
     },
 }
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
@@ -162,6 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
