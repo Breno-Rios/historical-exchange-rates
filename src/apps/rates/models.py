773a9108c14d76bccf3f6e3f5cf7be2defb5cffc
Breno Rios <brenooliveirarios@gmail.com>
2025-09-19 23:03:08 -0300

    refactor(rates): update Rate model fields (base, currency, value) and create migration

--- src/apps/rates/models.py
+++ src/apps/rates/models.py
@@ -1,12 +1,19 @@
 ### https://docs.djangoproject.com/en/5.2/ref/models/fields/#field-types < Referência para Criação dos tipos
 from django.db import models
+from config.settings.base import ALLOWED_CURRENCIES
 
 class Rate (models.Model):
 
-    date = models.DateField()
-    base = models.CharField(max_length=3)  
-    currency = models.CharField(max_length=3)  
-    value = models.FloatField()  
+    date = models.DateField(null=False,blank=False)
+    base = models.CharField(max_length=3,null=False,blank=False,default='USD')  
+    currency = models.CharField(
+        max_length=3,
+        choices=[(c, c) for c in ALLOWED_CURRENCIES],
+        unique=True,
+        null=False,
+        blank=False
+    )
+    value = models.DecimalField(null=True,blank=False,decimal_places= 4, max_digits=10)  
 
     def __str__(self):
         return f'{self.date} | {self.base} → {self.currency} = {self.value}'
