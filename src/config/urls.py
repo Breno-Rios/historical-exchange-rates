e3b7eb7dd1b2b108ffe2f47cd8aef7ed849d4636
Breno Rios <brenooliveirarios@gmail.com>
2025-09-27 15:01:22 -0300

    feat(urls) add a base url to api endpoints

--- src/config/urls.py
+++ src/config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.rates.urls')),
+    path('api/v1/exchange-rates/', include('apps.api.routers.api_get_routers.urls'))
 ]
