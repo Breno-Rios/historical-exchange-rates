a5f0da09983f1cb5d4f97a11ecfe1948b2092ffb
Breno Rios <brenooliveirarios@gmail.com>
2025-09-28 16:20:12 -0300

    refactor(views): integrate ExchangeRatesSerializer in currency exchange views

--- src/apps/api/views.py
+++ src/apps/api/views.py
@@ -6,90 +6,107 @@ from .services.exchange_rates_services import RateService
 
 from .validators.currency_code_validator import CurrencyValidator
 
+from .serializers.exchange_rates_serializer import ExchangeRatesSerializer
+
+
 def get_all_currency_exchange_rates(request):
     if request.method == 'GET':
         try:
 
-            content = RateService.get_all_currency_exchange_rates()
-            
-            return JsonResponse({"data": content}, safe= True, status= 200)
-        
-        except ObjectDoesNotExist as e:  
+            data = RateService.get_all_currency_exchange_rates()
+            content = ExchangeRatesSerializer.list_to_dict(data)
+
+            return JsonResponse({"data": content}, safe=True, status=200)
+
+        except ObjectDoesNotExist as e:
             return JsonResponse({"error": str(e)}, status=404)
 
-        except ValueError as e:  
+        except ValueError as e:
             return JsonResponse({"error": str(e)}, status=400)
 
-        except Exception as e: 
+        except Exception as e:
             return JsonResponse({"error": "Unexpected error", "details": str(e)}, status=500)
+
 
 def get_currency_exchange_rates_by_date(request):
     if request.method == 'GET':
         try:
-            exchange_date = DateFormatter.fromisoformat(request.GET.get('exchange-date'))
+            exchange_date = request.GET.get('exchange-date')
 
             if not exchange_date:
                 raise ValueError('missing exchange_date param')
-            
-            content = RateService.get_currency_exchange_rates_by_date(date= exchange_date)
-            
-            return JsonResponse({"data": content}, status= 200)
-        
-        except ObjectDoesNotExist as e:  
+
+            exchange_date = DateFormatter.fromisoformat(
+                request.GET.get('exchange-date'))
+
+            data = RateService.get_currency_exchange_rates_by_date(
+                date=exchange_date)
+            content = ExchangeRatesSerializer.list_to_dict(data)
+
+            return JsonResponse({"data": content}, status=200)
+
+        except ObjectDoesNotExist as e:
             return JsonResponse({"error": str(e)}, status=404)
 
-        except ValueError as e:  
+        except ValueError as e:
             return JsonResponse({"error": str(e)}, status=400)
 
-        except Exception as e: 
+        except Exception as e:
             return JsonResponse({"error": "Unexpected error", "details": str(e)}, status=500)
 
+
 def get_currency_exchange_rates_by_currency(request):
-     if request.method == 'GET':
+    if request.method == 'GET':
         try:
             currency_code = request.GET.get('currency-code')
 
             if not currency_code:
                 raise ValueError('missing currency_code param')
 
-            currency_code = CurrencyValidator.validate(request.GET.get('currency-code'))
+            currency_code = CurrencyValidator.validate(
+                request.GET.get('currency-code'))
 
-            
-            content = RateService.get_currency_exchange_rates_by_currency(target_currency = currency_code)
-            
-            return JsonResponse({"data": content}, status= 200)
-        
-        except ObjectDoesNotExist as e:  
+            data = RateService.get_currency_exchange_rates_by_currency(
+                target_currency=currency_code)
+            content = ExchangeRatesSerializer.list_to_dict(data)
+
+            return JsonResponse({"data": content}, status=200)
+
+        except ObjectDoesNotExist as e:
             return JsonResponse({"error": str(e)}, status=404)
 
-        except ValueError as e:  
+        except ValueError as e:
             return JsonResponse({"error": str(e)}, status=400)
 
-        except Exception as e: 
+        except Exception as e:
             return JsonResponse({"error": "Unexpected error", "details": str(e)}, status=500)
+
 
 def get_currency_exchange_rates_by_data_range(request):
 
     if request.method == 'GET':
         try:
-            start_date = DateFormatter.fromisoformat(request.GET.get('start-date'))
-            end_date = DateFormatter.fromisoformat(request.GET.get('end-date'))
+            start_date = request.GET.get('start-date')
+            end_date = request.GET.get('end-date')
 
             if not start_date or not end_date:
                 raise ValueError('missing start_date or end_date')
-            
-            content = RateService.get_currency_exchange_rates_by_data_range(start_date= start_date, end_date= end_date)
 
-            return JsonResponse({"data": content}, status= 200)
-        
-        except ObjectDoesNotExist as e:  
+            start_date = DateFormatter.fromisoformat(
+                request.GET.get('start-date'))
+            end_date = DateFormatter.fromisoformat(request.GET.get('end-date'))
+
+            data = RateService.get_currency_exchange_rates_by_data_range(
+                start_date=start_date, end_date=end_date)
+            content = ExchangeRatesSerializer.list_to_dict(data)
+
+            return JsonResponse({"data": content}, status=200)
+
+        except ObjectDoesNotExist as e:
             return JsonResponse({"error": str(e)}, status=404)
 
-        except ValueError as e:  
+        except ValueError as e:
             return JsonResponse({"error": str(e)}, status=400)
 
-        except Exception as e: 
+        except Exception as e:
             return JsonResponse({"error": "Unexpected error", "details": str(e)}, status=500)
-        
-
-    
