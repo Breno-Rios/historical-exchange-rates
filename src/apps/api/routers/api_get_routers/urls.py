50416b0bcbe715cee4cf0bb6a151d29bda352e9c
Breno Rios <brenooliveirarios@gmail.com>
2025-09-28 18:18:39 -0300

    refactor(urls): add 'all/' endpoint to return all exchange rates

--- src/apps/api/routers/api_get_routers/urls.py
+++ src/apps/api/routers/api_get_routers/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('period/', views.get_currency_exchange_rates_by_data_range, name= "get_currency_exchange_rates_by_data_range"),
     path('date/', views.get_currency_exchange_rates_by_date, name= "get_currency_exchange_rates_by_date"),
     path('exchange-code/', views.get_currency_exchange_rates_by_currency, name= "get_currency_exchange_rates_by_currency"),
-    path('', views.get_all_currency_exchange_rates, name= "get_all_currency_exchange_rates"),
+    path('all', views.get_all_currency_exchange_rates, name= "get_all_currency_exchange_rates"),
 ]
