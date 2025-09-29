40e1b2fc1c4b8eda8930fcf1cdfda187fd903355
Breno Rios <brenooliveirarios@gmail.com>
2025-09-29 19:58:30 -0300

    refactor(utils): move utils folder to project-level

---
+++ src/apps/utils/date_generator.py
@@ -0,0 +1,24 @@
+
+from datetime import datetime,timedelta
+
+class DateGenerator():
+    
+    @staticmethod
+    def range_dates(start: datetime , end: datetime):
+
+        if not isinstance(start, datetime) and isinstance(end, datetime):
+            raise ValueError('error: the types of dates must be a datetime')
+
+        date_list = []
+        current_date = start
+
+        while current_date <= end:
+            date_list.append(current_date)  
+            current_date += timedelta(days=1)
+        return date_list
+
+    
+    @staticmethod
+    def range_iso_dates(start: datetime, end: datetime):
+        return [d.isoformat() for d in DateGenerator.range_dates(start, end)]
+    
