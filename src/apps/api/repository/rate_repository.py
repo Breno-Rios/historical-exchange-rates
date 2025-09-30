e78761d8e85efa2588f3953663761809723baa12
Breno Rios <brenooliveirarios@gmail.com>
2025-09-29 21:17:37 -0300

    feat(repository): create RateRepository

---
+++ src/apps/api/repository/rate_repository.py
@@ -0,0 +1,33 @@
+from apps.rates.models import Rate
+from datetime import datetime
+from django.core.exceptions import ObjectDoesNotExist
+
+class RateRepository:
+
+    @staticmethod
+    def find_all():
+        return Rate.objects.all()
+
+    @staticmethod
+    def find_by_date(date: datetime):
+        return Rate.objects.filter(date=date)
+
+    @staticmethod
+    def find_by_currency(target_currency: str):
+        return Rate.objects.filter(currency= target_currency)
+
+    @staticmethod
+    def find_by_data_range(start_date: datetime, end_date: datetime):
+        return Rate.objects.filter( date__range=(start_date, end_date))
+    
+    @staticmethod
+    def insert(date, base, currency, value):
+        Rate.objects.create(
+                date=date,
+                base=base,
+                currency=currency,
+                value=value
+            )
+
+
+
