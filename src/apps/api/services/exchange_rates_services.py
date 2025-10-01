f3984dccd153789a0072ea03093ef1810d83d113
Breno Rios <brenooliveirarios@gmail.com>
2025-10-01 20:50:47 -0300

    feat(service): add methods insert_external_obj_exchange_rates and get_dashboard_currency_exchange_rates to RateService

--- src/apps/api/services/exchange_rates_services.py
+++ src/apps/api/services/exchange_rates_services.py
@@ -1,10 +1,63 @@
-from apps.rates.models import Rate
 from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
 from apps.api.repository.rate_repository import RateRepository
+from apps.utils.date_generator import DateGenerator
+from apps.api.external_api import api_vatcomply_com_rates
 
 
 class RateService:
+
+    @staticmethod
+    def insert_external_obj_exchange_rates(dates: list, base_currency:str, target_currency: str):
+
+        for date in dates:
+
+            params={
+                "base": base_currency,
+                "symbols": target_currency,
+                "date": date
+            }
+            response = api_vatcomply_com_rates.get_vatcomply_exchange_rates(params=params)
+            data = response.json()
+
+            RateRepository.insert(date= data.get('date'),
+                                  base=data.get('base'),
+                                  currency=list(data.get('rates'))[0],
+                                  value=data.get('rates').get('BRL')
+                                  )
+
+    @staticmethod
+    def get_dashboard_currency_exchange_rates(start_date: datetime, end_date: datetime, base_currency:str, target_currency: str):
+        try:
+            all_dates = DateGenerator.range_dates(start_date, end_date)
+
+            rates_queryset = RateRepository.find_by_data_range_and_filters(
+                start_date,
+                end_date,
+                base= base_currency,
+                currency= target_currency
+            )
+
+            existing_dates = rates_queryset.values_list("date", flat=True)
+            missing_dates = [d for d in all_dates if d not in existing_dates]
+
+            if not missing_dates:
+                return list(rates_queryset)
+            
+            RateService.insert_external_obj_exchange_rates(missing_dates, base_currency, target_currency)
+
+            return list(
+                        RateRepository.find_by_data_range_and_filters(
+                            start_date,
+                            end_date,
+                            base= base_currency,
+                            currency= target_currency
+                            )
+                        )
+    
+        except ObjectDoesNotExist as e:
+            raise (f'error: {e}')
+
 
     @staticmethod
     def get_all_currency_exchange_rates():
