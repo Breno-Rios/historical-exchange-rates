2dd2b8aa0ab00e5a887f9b09198aca8c82e56eb4
Breno Rios <brenooliveirarios@gmail.com>
2025-10-04 16:59:16 -0300

    feat(js): add fetch for dashboard exchange rates

---
+++ src/apps/rates/static/rates/js/fetch-data.js
@@ -0,0 +1,54 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const form = document.getElementById('rates-form')
+
+    form.addEventListener('submit', async function(event){
+        event.preventDefault();
+
+        const startDate = form.querySelector('[name="start_date"]').value;
+        const endDate = form.querySelector('[name="end_date"]').value;
+        const currencyCode = form.querySelector('[name="currency"]').value;
+        
+        const url = `/dashboard/?start-date=${startDate}&end-date=${endDate}&currency-code=${currencyCode}`;
+        
+        try{
+            const response = await fetch(url);
+            const jsonData = await response.json()
+
+            if(response.ok){
+                console.log(jsonData)
+                updateChart(jsonData.data)
+            }else{
+                alert(jsonData.error)
+                console.log(jsonData.error)
+            }
+            
+        }catch (e){
+            console.log('Error to request:', e)
+        }
+
+    })
+
+
+    function updateChart(data) {
+        if (!data || !data.length) {
+            console.warn("No data received to update chart");
+            return;
+        }
+        
+        const categories = data.map(d => d.date);
+        const values = data.map(d => parseFloat(d.value) || 0);
+        const currencyStr = data.map(d => d.base_code)
+        
+        window.chart.xAxis[0].setCategories(categories);
+        window.chart.series[0].setData(values);
+
+        chart.update({
+            tooltip: {
+                pointFormat: `{point.y} ${currencyStr[0]}`
+            }
+        });
+    }
+
+})
