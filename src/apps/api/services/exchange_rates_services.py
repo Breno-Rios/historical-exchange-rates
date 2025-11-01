ac67e4f937271b9c07948271ee86165f675b646c
Breno Rios <brenooliveirarios@gmail.com>
2025-10-31 21:04:27 -0300

    refactor(services): integrate client for exchange rates and change redundant methods

--- src/apps/api/services/exchange_rates_services.py
+++ src/apps/api/services/exchange_rates_services.py
@@ -3,8 +3,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from apps.api.repository.rate_repository import RateRepository
 from apps.utils.date_generator import DateGenerator
 from apps.utils.workday import filter_only_five_workdays
-from apps.api.external_api import api_vatcomply_com_rates
-
+from apps.api.clients.api_vatcomply import ClientVatcomply
+from apps.rates.models import Rate
+from datetime import date
+from decimal import Decimal
 
 class RateService:
     def __init__(self, repository: RateRepository):
@@ -12,34 +14,24 @@ class RateService:
 
     def __insert_external_obj_exchange_rates(self, dates: list, base_currency:str, target_currency: str):
         
-        for date in dates:
+        client =  ClientVatcomply.fetch_by_dates(base_currency,target_currency,dates)
 
-            params={
-                "base": base_currency,
-                "symbols": target_currency,
-                "date": date
-            }
-            response = api_vatcomply_com_rates.get_vatcomply_exchange_rates(params=params)
-            data = response.json()
-            
-            currency = list(data.get('rates'))[0]
-            self.repository.insert(date= data.get('date'),
-                                  base=data.get('base'),
-                                  currency=currency,
-                                  value=data.get('rates').get(currency)
-                                  )
+        for data in client:
+            self.repository.insert(
+                date=data['date'],
+                base=data['base'],
+                currency=list(data['rates'].keys())[0],
+                value=list(data['rates'].values())[0]
+            )
 
     def get_dashboard_currency_exchange_rates(self, start_date: datetime, end_date: datetime, base_currency:str, target_currency: str):
         try:
             
             all_dates = filter_only_five_workdays(DateGenerator.range_dates(start_date, end_date))
 
-            rates_queryset = self.repository.find_by_data_range_and_filters(
-                start_date,
-                end_date,
-                base= base_currency,
-                currency= target_currency
-            )
+            filters = dict(start_date = start_date, end_date= end_date, base = base_currency, currency = target_currency)
+
+            rates_queryset = self.find_by_data_range_and_filters(**filters)
 
             existing_dates = rates_queryset.values_list("date", flat=True)
             missing_dates = [d for d in all_dates if d not in existing_dates]
@@ -49,44 +41,33 @@ class RateService:
             
             self.__insert_external_obj_exchange_rates(missing_dates, base_currency, target_currency)
             
-            return self.repository.find_by_data_range_and_filters(
-                            start_date,
-                            end_date,
-                            base= base_currency,
-                            currency= target_currency
-                            )
-                        
+            return self.find_by_data_range_and_filters(**filters)
+                           
     
         except ObjectDoesNotExist as e:
             raise (f'error: {e}')
 
 
-    def get_all_currency_exchange_rates(self):
+    def find_all(self):
         try:
-            rates_list = self.repository.find_all()
-            return rates_list
-        except ObjectDoesNotExist as e:
+            queryset = self.repository.find_all()
+            return queryset
+        except queryset.ObjectDoesNotExist as e:
             raise (f'error: {e}')
-
-    def get_currency_exchange_rates_by_date(self, date: datetime):
+        
+    def find_by(self, **filters):
+       try:
+           queryset = self.repository.find_by(**filters) 
+           return queryset
+       except queryset.ObjectDoesNotExist as e:
+            raise (f'error: {e}')
+        
+    def find_by_data_range_and_filters(self, **filters):
         try:
-            rates_list = self.repository.find_by_date(date=date)
-            return rates_list
-        except ObjectDoesNotExist as e:
-            raise ObjectDoesNotExist(
-                f'error: {e}, No rates found for date {date}')
-
-    def get_currency_exchange_rates_by_currency(self, target_currency: str):
-        try:
-            rates_list = self.repository.find_by_currency(target_currency=target_currency)
-            return rates_list
-        except ObjectDoesNotExist as e:
-            raise ObjectDoesNotExist(
-                f'error: {e}, No rates found for currency {target_currency}')
-
-    def get_currency_exchange_rates_by_data_range(self, start_date: datetime, end_date: datetime):
-        try:
-            rates_list = self.repository.find_by_data_range(start_date, end_date)
-            return rates_list
-        except ObjectDoesNotExist as e:
-            raise ObjectDoesNotExist('error: {e}, No rates found in period')
+           start = filters.pop('start_date')
+           end = filters.pop('end_date')
+           queryset = self.repository.find_by_data_range_and_filters(start_date=start, end_date=end, **filters) 
+           return queryset
+        except queryset.ObjectDoesNotExist as e:
+            raise (f'error: {e}')
+        
