8856536f64c1fee349a27358b3e8709d7b300f45
Breno Rios <brenooliveirarios@gmail.com>
2025-10-31 21:17:53 -0300

    refactor(routers): replace old routes with new structure

---
+++ src/apps/api/routers/api_urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from apps.api import api_views 
+from rest_framework.urlpatterns import format_suffix_patterns
+
+app_name = "api"
+
+urlpatterns = [
+    path("rates/", api_views.RateAPIList.as_view()),
+    path("rate/", api_views.RateAPIFilter.as_view()),
+    path("rate/period/", api_views.RateAPIFilterPeriod.as_view()),
+
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
