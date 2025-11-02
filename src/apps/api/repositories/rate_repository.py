00e22b4b70c61bf5537eeadf1905ebde531a994d
Breno Rios <brenooliveirarios@gmail.com>
2025-11-02 16:22:05 -0300

    fix(repositories) fix the correct name folder to 'repositories' and remove old methods

--- src/apps/api/repositories/rate_repository.py
+++ src/apps/api/repositories/rate_repository.py
@@ -1,20 +1,14 @@
 from apps.rates.models import Rate
 from datetime import datetime
 
-class RateRepository:
+class RateRepository: 
 
-    def find_all(self, ):
+    def find_all(self):
         return Rate.objects.all()
-
-    def find_by_date(self, date: datetime):
-        return Rate.objects.filter(date=date)
-
-    def find_by_currency(self, target_currency: str):
-        return Rate.objects.filter(currency= target_currency)
-
-    def find_by_data_range(self, start_date: datetime, end_date: datetime):
-        return Rate.objects.filter( date__range=(start_date, end_date))
     
+    def find_by(self, **filters):
+        return Rate.objects.filter(**filters)
+
     def find_by_data_range_and_filters(self, start_date: datetime, end_date: datetime,**filters):
         return Rate.objects.filter(
                     date__range=(start_date, end_date),
@@ -26,7 +20,9 @@ class RateRepository:
                 date=date,
                 base=base,
                 currency=currency,
-                value=value
+                defaults={
+                    "value": value
+                    }
             )
 
 
