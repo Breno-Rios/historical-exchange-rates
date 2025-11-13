a4270bfd8d8c8610b0e90157ec7cee1cfbbf255e
Breno Rios <brenooliveirarios@gmail.com>
2025-11-12 21:32:14 -0300

    refactor(views) implemeted APIView class from DRF

--- src/apps/rates/views.py
+++ src/apps/rates/views.py
@@ -1,18 +1,19 @@
 from .forms import RateForms
 
-from config.settings.base import ALLOWED_CURRENCIES
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.exceptions import ValidationError
 
-from django.http import JsonResponse
-from datetime import date
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from apps.api.serializers.exchange_rates_serializer import ExchangeRatesSerializer
-from apps.api.services.exchange_rates_services import RateService
-from apps.api.repository.rate_repository import RateRepository
+from apps.api.services.exchange_rates_services import RateAPIService, RateDashboardService
+from apps.api.repositories.rate_repository import RateRepository
+from apps.api.serializers.api_serializer import RateSerializerList, RateSerializerFilterPeriod
 
-from requests.exceptions import Timeout, ConnectionError, HTTPError
 from django.core.exceptions import ObjectDoesNotExist
 
-from django.views.generic import TemplateView , View
+from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
@@ -22,64 +23,40 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["form"] = RateForms()
         return context
-
-class DashboardView(View):
-    service = RateService(RateRepository())
-    serializer = ExchangeRatesSerializer()
-
-    def json_error(self, message, status=400):
-        return JsonResponse({"error": message}, status=status)
     
-    def get_validated_params(self, request):
-            
-        start_date = request.GET.get('start-date')
-        end_date = request.GET.get('end-date')
-        currency_code = request.GET.get('currency-code')
 
-        if not start_date or not end_date:
-            raise ValueError('missing start_date or end_date')
-        if not currency_code:
-            raise ValueError('missing currency_code param')
-
-        if currency_code not in ALLOWED_CURRENCIES:
-            raise ValueError(f"Currency '{currency_code}' is not supported. Only {ALLOWED_CURRENCIES}")
-        
-        start_date = date.fromisoformat(start_date)
-        end_date = date.fromisoformat(end_date)
-
-        return start_date, end_date, currency_code
-         
-    
-    def get(self, request, *args, **kwargs):
+class DashboardFilterPeriod(APIView): 
+    repository = RateRepository()
+    service = RateAPIService(repository)
+    dashboard_service = RateDashboardService(repository=repository,rate_api=service) 
+      
+    def get(self, request, start_date,end_date):
         try:
-            start_date, end_date, currency_code = self.get_validated_params(request)
-                
-            queryset = self.service.get_dashboard_currency_exchange_rates(
-                start_date= start_date,
-                end_date= end_date,
-                base_currency='USD',
-                target_currency=currency_code
-            )
-            content = self.serializer.list_to_dict(queryset)
-            return JsonResponse({"data": content}, status=200)
+            data = {
+                "start_date": start_date,
+                "end_date": end_date,
+                **request.query_params.dict()
+            }
 
-        except Timeout as e:
-            return self.json_error("Timeout contacting external API",504)
+            serializer = RateSerializerFilterPeriod(data=data)
+            serializer.is_valid(raise_exception=True)
+            
+            filters = serializer.validated_data
+            rate = self.dashboard_service.get_dashboard_currency_exchange_rates(**filters)
 
-        except ConnectionError as e:
-            return self.json_error("Could not connect to external API",503)
 
-        except HTTPError as e:
-            return self.json_error("HTTP error from external API",502)
+            serializer = RateSerializerList(rate, many = True)
 
-        except ObjectDoesNotExist as e:
-            return self.json_error(str(e),404)
-
+            return(Response(serializer.data, status=status.HTTP_200_OK))
+        
         except ValueError as e:
-            return self.json_error(str(e),400)
-
-        except RuntimeError as e:  
-            return self.json_error(str(e),500)
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except ObjectDoesNotExist as e:
+            return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as e:
-            return self.json_error(f"Unexpected error {str(e)}",500)
+            return Response({"error": "Unexpected error", "details": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
