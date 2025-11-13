e148a195fd39125396c3718df80fd5c5593baeba
Breno Rios <brenooliveirarios@gmail.com>
2025-11-12 21:25:40 -0300

    refactor(views) to get and check the params

--- src/apps/api/api_views.py
+++ src/apps/api/api_views.py
@@ -5,17 +5,25 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.api.services.exchange_rates_services import RateService
+from apps.api.services.exchange_rates_services import RateAPIService
 from apps.api.repositories.rate_repository import RateRepository
 from apps.api.serializers.api_serializer import RateSerializerList, RateSerializerFilter, RateSerializerFilterPeriod
 
 class RateAPIList(APIView):
     repository = RateRepository()
-    service = RateService(repository) 
+    service = RateAPIService(repository) 
 
     def get(self, request, format = None):
         try:
-            rates = self.service.find_all()
+            if request.query_params:
+                serializer = RateSerializerFilter(data=request.query_params)
+                serializer.is_valid(raise_exception=True)
+                
+                filters = serializer.validated_data
+                rates = self.service.find_by(**filters)
+            else:
+                rates = self.service.find_all()
+
             serializer = RateSerializerList(rates, many = True)
             return(Response(serializer.data, status=status.HTTP_200_OK))
 
@@ -28,46 +36,27 @@ class RateAPIList(APIView):
         except Exception as e:
             return Response({"error": "Unexpected error", "details": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-class RateAPIFilter(APIView):
-    repository = RateRepository()
-    service = RateService(repository) 
-      
-    def get(self, request, format = None):
-        try:
-
-            serializer = RateSerializerFilter(data=request.query_params)
-            serializer.is_valid(raise_exception=True)
-            
-            filters = serializer.validated_data
-            rate = self.service.find_by(**filters)
-            serializer = RateSerializerFilter(rate, many = True)
-
-            return(Response(serializer.data, status=status.HTTP_200_OK))
-        
-        except ValueError as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-                
-        except ValidationError as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        
-        except ObjectDoesNotExist as e:
-            return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
-
-        except Exception as e:
-            return Response({"error": "Unexpected error", "details": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 class RateAPIFilterPeriod(APIView): 
     repository = RateRepository()
-    service = RateService(repository) 
+    service = RateAPIService(repository) 
       
-    def get(self, request, format = None):
+    def get(self, request, start_date,end_date):
         try:
-            serializer = RateSerializerFilterPeriod(data=request.query_params)
+            data = {
+                "start_date": start_date,
+                "end_date": end_date,
+                **request.query_params.dict()
+            }
+
+            serializer = RateSerializerFilterPeriod(data=data)
             serializer.is_valid(raise_exception=True)
             
             filters = serializer.validated_data
             rate = self.service.find_by_data_range_and_filters(**filters)
-            serializer = RateSerializerFilterPeriod(rate, many = True)
+
+
+            serializer = RateSerializerList(rate, many = True)
 
             return(Response(serializer.data, status=status.HTTP_200_OK))
         
